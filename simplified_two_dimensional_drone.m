6a8fe62ef3c2bff5e44318b76cdf3fe0303bce2e
hoangt1215 <hoangt1215@gmail.com>
2018-06-22 22:47:12 -0400

    Simple 2-D dynamic

---
+++ simplified_two_dimensional_drone.m
@@ -0,0 +1,27 @@
+% two dimensional dynamics
+clc;
+clear;
+
+% initialize
+k = 0.01;
+h = k^2/2;
+T = 0.05;
+
+TimeSteps = round(T/h);
+XSpan = (0:k:1);
+
+u1 = 2*sin(pi/2*XSpan)
+u2 = sin(pi/2*XSpan)
+
+x = u1
+y = u2
+handle = plot(XSpan, x);
+
+for i = 1:TimeSteps
+  dtt = [[0, (x(1:end-2)-2*x(2:end-1)+x(3:end))/k^2, 0];[0, (y(1:end-2)-2*y(2:end-1)+y(3:end))/k^2, 0]]
+  x = x + h*dtt(1,:);
+  y = y + h*dtt(2,:);
+  set(handle, 'ydata', x);
+  title(['Time=',num2str(i*h)]);
+  drawnow();
+end
