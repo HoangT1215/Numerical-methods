ebec1e417ae3f2383f2ae31b586843203f18938f
hoangt1215 <hoangt1215@gmail.com>
2018-06-27 14:56:24 -0400

    Gradient descent with momentum

--- Code/gradient_descent_with_momentum.m
+++ Code/gradient_descent_with_momentum.m
@@ -5,26 +5,29 @@ clear;
 % initialize
 k = 0.01;
 h = 0.01;
-T = 0.05;
-gamma = 0.15;
+T = 3;
+gamma = 5;
 
 TimeSteps = round(T/h);
-XSpan = (0:k:1);
-N = length(SpaceSpan);
+XSpan = transpose([0:k:1]);
+N = length(XSpan)-1;
 
-u1 = sin(pi/2*XSpan)
-u2 = sin(pi/2*XSpan)
-L = (-2*diag(ones(N-1,1)) + diag(ones(N-2,1),-1) + diag(ones(N-2,1),1))/k^2;
+u1 = XSpan
+u2 = sin(5*pi/2*XSpan)
+L = (-2*diag(ones(N-1,1)) + diag(ones(N-2,1),-1) + diag(ones(N-2,1),1));
+
+b = zeros(N-1,1); % scalar value
+b(end) = h/(gamma*k^2);
 
 p = u1
 q = u2
-handle = plot(XSpan, x);
+handle = plot(XSpan, q);
 
 for i = 1:TimeSteps
-  p = 1/(1+gamma*h-(h/k)^2)*(p+h/k^2*q)
-  q = inv((1+gamma*h)*diag(ones(N,1)) - (h/k)^2*L)*(h*p+(1+gamma*h)*q)
+  p(2:end-1) = inv((1+gamma*h)*diag(ones(N-1,1)) - (h/k)^2*L)*(p(2:end-1)+h/k^2*L*(q(2:end-1)+b))
+  q(2:end-1) = inv((1+gamma*h)*diag(ones(N-1,1)) - (h/k)^2*L)*(h*p(2:end-1)+(1+gamma*h)*(q(2:end-1)+b))
   
-  set(handle, 'ydata', x);
+  set(handle, 'ydata', q);
   title(['Time=',num2str(i*h)]);
   drawnow();
 end
