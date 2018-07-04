510f1a1275f03ba795d597dfc21362e6da3fe975
hoangt1215 <hoangt1215@gmail.com>
2018-07-04 08:04:44 -0400

    Nesterov gradient descent implementation for heat equation

--- Code/accelerated_gradient_descent.m
+++ Code/accelerated_gradient_descent.m
@@ -1,14 +1,36 @@
-% Accelerated gradient descent
+% Accelerated gradient descent (heat equation)
 clc;
 clear;
 
 % initialize
-syms x,y
-f = (x-1)^2 + 100*(y-x^2)^2
-step = 0.01
-x0 = 0
-y0 = 0
+k = 0.01;
+h = 0.01;
+s = 0.000004;
+T = 15;
 
-% algorithm
-step = backtrackinglinesearch(f)
+TimeSteps = round(T/h);
+XSpan = transpose([0:k:1]);
+N = length(XSpan)-1;
 
+u1 = sin(0.5*pi*XSpan)
+u2 = sin(2.5*pi*XSpan)
+L = (-2*diag(ones(N-1,1)) + diag(ones(N-2,1),-1) + diag(ones(N-2,1),1));
+
+b = zeros(N-1,1); % scalar value
+b(end) = (s/k^2); % tentative value
+
+x = u1
+y = u2
+old_x = x
+handle = plot(XSpan, y);
+
+% iteration
+for t = 1:TimeSteps
+  x(2:end-1) = y(2:end-1) + s*L/k^2*(y(2:end-1)) + b
+  y = x + (t-1)/(t+2)*(x-old_x)
+  old_x = x
+  
+  set(handle, 'ydata', y);
+  title(['Time=',num2str(t*h)]);
+  drawnow();
+end
